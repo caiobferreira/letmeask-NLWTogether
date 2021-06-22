37eaa7315fd0fa36f8f5919f58d2c129f054a8e6
caiobferreira <caaio.ferreira@gmail.com>
2021-06-21 23:41:33 -0300

    AuthContext and Auth interface development

--- letmeask/src/components/Button.tsx
+++ letmeask/src/components/Button.tsx
@@ -1,10 +1,12 @@
+import {ButtonHTMLAttributes} from 'react';
 
-type ButtonProps = {
-    text?: string;
-}
+import '../styles/button.scss';
 
-export function Button(props: ButtonProps) {
-    return(
-        <button>{props.text || 'Default'}</button>
-    )
+type ButtonProps= ButtonHTMLAttributes<HTMLButtonElement>;
+
+
+export function Button(props: ButtonProps){
+  return (
+    <button className="button" {...props}/>
+  )
 }
